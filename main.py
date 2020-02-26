d388f7860c5b8b7c011dd426d3ec2ed4ab871ded
aleksander.vatov@gmail.com <5 Books>
2020-02-26 11:14:13 +0100

    Added documentation and a more meaningful example

--- main.py
+++ main.py
@@ -1,38 +1,7 @@
-# from pathfinding.core.diagonal_movement import DiagonalMovement
-from pathfinding.core.grid import Node
-# from pathfinding.finder.a_star import AStarFinder
-#
-# matrix = [
-#   [1, 1, 1],
-#   [1, 0, 1],
-#   [1, 1, 1]
-# ]
-# grid = Grid(matrix=matrix)
-#
-# start = grid.node(0, 0)
-# end = grid.node(2, 2)
-#
-# finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
-# path, runs = finder.find_path(start, end, grid)
-#
-# print('operations:', runs, 'path length:', len(path))
-# print(grid.grid_str(path=path, xsstart=start, end=end))
-
 from map import Map
 
-# test = '''####################
-# ##_____T____##___###
-# ####___X_#_______###
-# ######_T_##______###
-# #___TXTT~~##__++__##
-# #___T_#~~~~##+++++_#
-# ____T_#~~~~~#++++___
-# #______~~~~##+++___#
-# #_______~~#________#
-# ___HHHH*HH*HHHHH*___
-# ###__________#######'''
-
-
 m = Map(open('1_victoria_lake.txt','r'))
-for c in m.customers:
-    print(c.location, c.reward)
+print("Map contains",len(m.customers), "customers.")
+path = m.path(m.customers[0].location, m.customers[-1].location)
+print("The path between the first and last customer costs:", path.cost())
+print("It passes through the following points:",path.nodes)
--- map.py
+++ map.py
@@ -15,7 +15,20 @@ costs =  {
 }
 
 class Map:
+    """
+    Represents a world map.
+
+    Attributes:
+        customers(List[Map.Customer]): The customers in the world map
+        grid(pathfinding.core.grid.Grid): the underlying grid
+        finder(pathfinding.finder.a_star.AStarFinder): the A* finder used to find paths
+
+    """
     def __init__(self, source: Union[str, TextIO]):
+        """
+        Reads a new map from a file or a string
+        :param source: the file-like object or string describing the map (not a filename!)
+        """
         if isinstance(source, str):
             input_string = source
         else:
@@ -34,16 +47,32 @@ class Map:
         self.finder = AStarFinder()
 
     def path(self, start:Tuple[int,int], end:Tuple[int,int]) -> 'Map.Path':
+        """
+        Finds the cheapest path between start and end.
+        :param start: The start node, represented as an (x, y) tuple
+        :param end: The end node, represented as an (x, y) tuple
+        :return Map.Path: The path
+        """
         nodes = self.finder.find_path(self.grid.node(*start),self.grid.node(*end),self.grid)[0]
         return Map.Path(nodes, self.grid)
 
 
     class Path:
+        """
+        A path between two nodes in a particular map
+
+        Attributes:
+            nodes (List[Tuple[int,int]): A list of all points, including the start and end points
+            grid (pathfinding.core.grid.Grid) The grid of the map
+        """
         def __init__(self, nodes: List[Tuple[int,int]], grid: Grid):
             self.nodes = nodes
             self.grid = grid
 
         def __str__(self):
+            """
+            Converts the path to a string in the format specified by the challenge
+            """
             s = ''
             for i in range(len(self.nodes)-1):
                 x, y = self.nodes[i]
@@ -60,11 +89,22 @@ class Map:
             return s
 
         def cost(self) -> int:
+            """
+            Calculates the cost of the path, or -1 if no path exists
+            :return: The cost of the path as an int
+            """
             if len(self.nodes) == 0:
-                return 0
+                return -1
             else:
                 return sum((self.grid.node(x,y).weight for x, y in self.nodes[1:]))
 
     class Customer:
+        """
+        Represents a customer.
+
+        Attributes:
+            location (Tuple[int,int]): the location of the customer's headquarters
+            reward (int): the reward associated with this customer
+        """
         def __init__(self, location, reward):
             self.location, self. reward = location, reward
